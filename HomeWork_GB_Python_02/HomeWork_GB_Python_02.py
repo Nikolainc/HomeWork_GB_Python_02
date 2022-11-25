62b9abf49ed5de01321d6d8da0e8484eed2c63b0
nikolainc <nikolai9710@gmail.com>
2022-11-25 17:07:42 +0300

    Задача 3

--- HomeWork_GB_Python_02/HomeWork_GB_Python_02.py
+++ HomeWork_GB_Python_02/HomeWork_GB_Python_02.py
@@ -38,3 +38,53 @@
 #    count-=1
 #print(numberFirstMax)
 #print(numberSecondMax)
+
+#==============================================================
+
+#3) Вводим с клавиатуры целое число - это зарплата.
+#Нужно вывести в консоль -  Минимальное кол-во  купюр, которыми можно выдать ЗП.
+#И сколько, и каких бухгалтер выдаст 25 рублевых купюр,  10 рублевых, 3 рублевых и 1 рублевых
+
+#print("Введите зарплату:")
+#salary = int(input())
+
+#if (salary / 5000) >= 1:
+#   Banknote5000Counter = int(salary / 5000)
+#   salary -= 5000 * Banknote5000Counter
+#   print(f"Потребуется {Banknote5000Counter} - 5000 банкнот")
+#if (salary / 2000) >= 1:
+#   Banknote2000Counter = int(salary / 2000)
+#   salary -= 2000 * Banknote2000Counter
+#   print(f"Потребуется {Banknote2000Counter} - 2000 банкнот")
+#if (salary / 1000) >= 1:
+#   Banknote1000Counter = int(salary / 1000)
+#   salary -= 1000 * Banknote1000Counter
+#   print(f"Потребуется {Banknote1000Counter} - 1000 банкнот")
+#if (salary / 500) >= 1:
+#   Banknote500Counter = int(salary / 500)
+#   salary -= 500 * Banknote500Counter
+#   print(f"Потребуется {Banknote500Counter} - 500 банкнот")
+#if (salary / 200) >= 1:
+#   Banknote200Counter = int(salary / 200)
+#   salary -= 200 * Banknote200Counter
+#   print(f"Потребуется {Banknote200Counter} - 200 банкнот")
+#if (salary / 100) >= 1:
+#   Banknote100Counter = int(salary / 100)
+#   salary -= 100 * Banknote100Counter
+#   print(f"Потребуется {Banknote100Counter} - 100 банкнот")
+#if (salary / 50) >= 1:
+#   Banknote50Counter = int(salary / 50)
+#   salary -= 50 * Banknote50Counter
+#   print(f"Потребуется {Banknote50Counter} - 50 банкнот")
+#if (salary / 10) >= 1:
+#   Banknote10Counter = int(salary / 10)
+#   salary -= 10 * Banknote10Counter
+#   print(f"Потребуется {Banknote10Counter} - 10 банкнот")
+#if (salary / 5) >= 1:
+#   Banknote5Counter = int(salary / 5)
+#   salary -= 5 * Banknote5Counter
+#   print(f"Потребуется {Banknote5Counter} - 5 банкнот")
+#if (salary / 1) >= 1:
+#   Banknote1Counter = int(salary / 1)
+#   salary -= 1 * Banknote1Counter
+#   print(f"Потребуется {Banknote1Counter} - 1 банкнот")
