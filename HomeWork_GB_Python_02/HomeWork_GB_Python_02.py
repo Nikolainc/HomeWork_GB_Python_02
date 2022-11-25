c11a18b1ffdff14896b225e37e2ec0a5b5e27df2
nikolainc <nikolai9710@gmail.com>
2022-11-25 16:32:42 +0300

    Задача 1

--- HomeWork_GB_Python_02/HomeWork_GB_Python_02.py
+++ HomeWork_GB_Python_02/HomeWork_GB_Python_02.py
@@ -1,1 +1,22 @@
-﻿print("Hello")
+﻿#1) Вводим с клавиатуры целое число X и У.
+#Выводим на экран количество чисел между Х и У, которые делятся на 2 и 3
+
+
+print("Введите число X:")
+numberStart = int(input())
+print("Введите число Y:")
+numberLast = int(input())
+
+if numberLast < numberStart:
+    numberLast, numberStart = numberStart, numberLast
+
+counter = 0
+
+while numberStart <= numberLast:
+    if numberStart % 2 == 0 and numberStart % 3 == 0:
+        counter+=1
+    numberStart+=1
+
+print(counter)
+
+#==============================================================
